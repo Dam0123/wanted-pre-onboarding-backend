0e98ffab0f74527de657e5bce106a2ae13343200
Dam0123 <hwadam1020@naver.com>
2023-10-09 00:38:16 +0900

    update: add validation rule to PostRequest
    PostRequest의 companyId 항목에 정규식 규칙 추가

--- src/main/java/com/wanted/post/dto/PostRequest.java
+++ src/main/java/com/wanted/post/dto/PostRequest.java
@@ -4,10 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
+import javax.validation.constraints.*;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -33,5 +30,6 @@ public class PostRequest {
     private String deadline;
 
     @NotNull(message = "companyId는 필수 입력 항목입니다.")
+    @Positive(message = "companyId는 1 이상의 양수만 가능합니다.")
     private Long companyId;
 }
